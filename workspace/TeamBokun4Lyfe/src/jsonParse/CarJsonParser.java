5582bfd11a3e1432d21ef638b6b059c2cdb84c85
Christopher O'Brien <christopherhenryobrien@gmail.com>
2014-04-18 22:15:01 -0700

    Reading from HTTP Server

--- workspace/TeamBokun4Lyfe/src/jsonParse/CarJsonParser.java
+++ workspace/TeamBokun4Lyfe/src/jsonParse/CarJsonParser.java
@@ -1,8 +1,15 @@
 package jsonParse;
+import java.awt.List;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -11,23 +18,45 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
 import data.Car;
+import data.RoadSegment;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class CarJsonParser {
+	
 	private ArrayList<Car> cars = new ArrayList<Car>();
 	
+	List jsonFile;
+	
 	//	Pass in path of json file
-	public CarJsonParser(String jsonPath) {
+	public CarJsonParser() {
+		
+//		String jsonPath = pullData();
+		
 		//		Configure Gson
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		gsonBuilder.registerTypeAdapter(Car.class, new CarDeserializer());
 		Gson gson = gsonBuilder.create();
 		
 		BufferedReader br = null;
+		
 		try {
-			br = new BufferedReader(new FileReader(jsonPath));
+			
+		    URL                url; 
+		    URLConnection      connection; 
+		    DataInputStream    dataStreamer;
+
+		    url = new URL("http://www-scf.usc.edu/~csci201/mahdi_project/project_data.json");
+
+		    connection = url.openConnection(); 
+		    connection.setDoInput(true); 
+		    connection.setUseCaches(false);
+
+		    dataStreamer = new DataInputStream(connection.getInputStream()); 
+		    br = new BufferedReader(new InputStreamReader(dataStreamer));
+			
+//			br = new BufferedReader(new FileReader(jsonPath));
 			String jsonString = br.readLine();
 			if(jsonString == null) {
 				System.err.println("Invalid JSON file.");
@@ -71,7 +100,45 @@ public class CarJsonParser {
 			System.out.println(cars.get(i));
 	}
 	
+//	public List pullData()
+//	{
+//		
+//		String jsonFile;
+//		
+//		try
+//		{
+//		    URL                url; 
+//		    URLConnection      connection; 
+//		    DataInputStream    dataStreamer;
+//
+//		    url = new URL("http://www-scf.usc.edu/~csci201/mahdi_project/project_data.json");
+//
+//		    connection = url.openConnection(); 
+//		    connection.setDoInput(true); 
+//		    connection.setUseCaches(false);
+//
+//		    dataStreamer = new DataInputStream(connection.getInputStream()); 
+//		    BufferedReader br = new BufferedReader(new InputStreamReader(dataStreamer));
+//		    String temp; 
+//		  
+//		    jsonFile.clear();
+//		  
+//		    while ((temp = br.readLine()) != null)
+//		    { 
+//		      jsonFile
+//		    } 
+//		      dataStreamer.close(); 
+//		    }
+//
+//	    	catch (MalformedURLException mue) {} 
+//	    	catch (IOException ioe) {}
+//		
+//			return jsonFile;
+//	}
+
+	
 	public static void main(String[] args) {
-		new CarJsonParser("./ref/test.json").printCars();
+		new CarJsonParser().printCars();
 	}
+	
 }
